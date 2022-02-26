7d6442c0130bb43f508238077c6c38713918f185
venzel <eneas.eng@yahoo.com>
2022-02-26 01:08:50 -0300

    feat: setup do projeto

---
+++ src/components/UserList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="users">
+        <ul>
+            <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserList',
+    props: {
+        users: {
+            type: Array,
+            default: [],
+        },
+    },
+};
+</script>
+
+<style></style>
---
+++ src/views/User.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <UserList :users="[...users]" />
+    </div>
+</template>
+
+<script>
+import UserList from '../components/UserList.vue';
+export default {
+    components: { UserList },
+    data() {
+        return {
+            users: [],
+            userActivated: {},
+        };
+    },
+    mounted() {
+        const users = [
+            { id: 10, name: 'Tiago' },
+            { id: 20, name: 'Marcos' },
+        ];
+
+        this.users = users;
+    },
+};
+</script>
+
+<style></style>
